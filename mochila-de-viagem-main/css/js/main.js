d1059b8d1f2cb37254f1ccfecc31ad7853b758a9
Andre <devals2022@gmail.com>
2022-10-25 23:58:02 -0300

    atualiza

--- mochila-de-viagem-main/css/js/main.js
+++ mochila-de-viagem-main/css/js/main.js
@@ -12,29 +12,24 @@ itens.forEach((elemento) => {
 form.addEventListener('submit', (evento) =>{
     evento.preventDefault();
    
-    const nome = evento.target.elements['nome'];
-    const quantidade = evento.target.elements['quantidade'];
-    const existe = itens.find(elemento => elemento.nome === nome.value);
+    const nome = evento.target.elements["nome"];
+    const quantidade = evento.target.elements["quantidade"];
+    const existe = itens.find(elemento => elemento.nome === nome.value)
 
     const itemAtual= {
-        nome:nome.value,
-        quantidade:quantidade.value
+        "nome":nome.value,
+        "quantidade":quantidade.value
         
     }
 
-    if(existe){
-
+    if (existe) {
         itemAtual.id = existe.id
         atualizaElemento(itemAtual);
-
-    }else{
-
-        itemAtual.id = itens.lenght;
-
-        criaElemento(itemAtual);
-
-       itens.push(itemAtual)
-
+        
+    } else {
+        itemAtual.id = itens.lenght 
+        criaElemento(itemAtual)  
+        itens.push(itemAtual)
     }
 
 localStorage.setItem("itens", JSON.stringify(itens));
@@ -49,20 +44,20 @@ function criaElemento(item){
     
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
- 
-    const numeroItem = document.createElement('strong');
+
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
-    console.log(numeroItem)
+    novoItem.appendChild(numeroItem)
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
-  
+
     lista.appendChild(novoItem);
 
 }
 
 function atualizaElemento(item){
-  document.querySelector("[data-id='"+ item.id +"']").innerHTML= item.quantidade;
+  document.querySelector("[data-id='" + item.id + "']").innerHTML= item.quantidade;
 
 }
