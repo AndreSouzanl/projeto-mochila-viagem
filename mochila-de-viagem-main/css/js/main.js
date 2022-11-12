919487587c08b4e0e6883ec611f02e160e0e9f4f
Andre <devals2022@gmail.com>
2022-11-12 16:52:18 -0300

    atualizando

--- mochila-de-viagem-main/css/js/main.js
+++ mochila-de-viagem-main/css/js/main.js
@@ -8,26 +8,30 @@ itens.forEach((elemento) => {
     criaElemento(elemento);
 
 })
+
+
  
 form.addEventListener('submit', (evento) =>{
     evento.preventDefault();
    
     const nome = evento.target.elements["nome"];
     const quantidade = evento.target.elements["quantidade"];
-    const existe = itens.find(elemento => elemento.nome === nome.value)
-
-    const itemAtual= {
+    const existe = itens.find( elemento => elemento.nome === nome.value)
+    
+    
+   
+     const itemAtual= {
         "nome":nome.value,
         "quantidade":quantidade.value
         
     }
-
     if (existe) {
         itemAtual.id = existe.id
         atualizaElemento(itemAtual);
-        
-    } else {
-        itemAtual.id = itens.lenght 
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+
+       } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
         criaElemento(itemAtual)  
         itens.push(itemAtual)
     }
@@ -53,11 +57,34 @@ function criaElemento(item){
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
     lista.appendChild(novoItem);
 
 }
 
 function atualizaElemento(item){
-  document.querySelector("[data-id='" + item.id + "']").innerHTML= item.quantidade;
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "x";
+    
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    })
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id){
+     tag.remove();
+    
+     itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+     localStorage.setItem("itens", JSON.stringify(itens));
+     
+     
 
 }
